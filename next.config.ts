7c6ea3c4bab35edbc160ae8cee9da27f7590aa32
Kebhino <wrobeljakub@gmail.com>
2025-07-03 00:22:54 +0200

    poprawka

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    DYNAMO_TABLE: process.env.DYNAMO_TABLE,
+    DYNAMO_WAGA_TABLE: process.env.DYNAMO_WAGA_TABLE,
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
+    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  },
 };
 
 export default nextConfig;
