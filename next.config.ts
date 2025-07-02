0746440e7f6c63c4ca3209dd42428e91ba953a65
Kebhino <wrobeljakub@gmail.com>
2025-07-03 01:12:31 +0200

    comits

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DYNAMO_TABLE: process.env.DYNAMO_TABLE,
-    DYNAMO_WAGA_TABLE: process.env.DYNAMO_WAGA_TABLE,
-    MY_AWS_REGION: process.env.MY_AWS_REGION,
     MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
     MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY,
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    DYNAMO_WAGA_TABLE: process.env.DYNAMO_WAGA_TABLE,
+    DYNAMO_TABLE: process.env.DYNAMO_TABLE,
   },
 };
 
