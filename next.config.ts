209164cbfc3d68ab51bfb80cb28793d2916fdb8a
Kebhino <wrobeljakub@gmail.com>
2025-07-03 01:27:51 +0200

    poprawka

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
-    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY,
-    MY_AWS_REGION: process.env.MY_AWS_REGION,
-    DYNAMO_WAGA_TABLE: process.env.DYNAMO_WAGA_TABLE,
-    DYNAMO_TABLE: process.env.DYNAMO_TABLE,
+    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID ?? "",
+    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY ?? "",
+    MY_AWS_REGION: process.env.MY_AWS_REGION ?? "eu-central-1",
+    DYNAMO_WAGA_TABLE: process.env.DYNAMO_WAGA_TABLE ?? "Waga",
+    DYNAMO_TABLE: process.env.DYNAMO_TABLE ?? "Posilki",
   },
 };
 
